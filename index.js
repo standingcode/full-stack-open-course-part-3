71dd496ed915a7bfcc10570d8ddbc83ae238ae4e
Paul Standing <paultobiasstanding@gmail.com>
2024-12-17 11:21:05 +0200

    Step 3.2 complete

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ app.get("/api/persons", (request, response) => {
   response.json(entries);
 });
 
+app.get("/info", (request, response) => {
+    response.send(`<p>Phonebook has info for ${entries.length} people</p><p>${new Date()}</p>`);
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
