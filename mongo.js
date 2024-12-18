7b0ad3739ca3dc1dd48e09b076f59d2a5c8a3714
Paul Standing <paultobiasstanding@gmail.com>
2024-12-18 15:13:38 +0200

    Exercise 3.12 command line database

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as an argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://phonebook-read-write:${password}@cluster0.vngt9eu.mongodb.net/PhonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length < 4) {
+  Person.find({})
+    .then((result) => {
+      result.forEach((note) => {
+        console.log(note);
+      });
+      mongoose.connection.close();
+      process.exit(1);
+    })
+    .catch((error) => {
+      process.exit(1);
+    });
+} else {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then((result) => {
+    console.log("person saved!");
+    console.log(result);
+    mongoose.connection.close();
+  });
+}
