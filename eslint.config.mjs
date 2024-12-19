e12683346ba1c2c56cdcab5541af1fe945d424e2
Paul Standing <paultobiasstanding@gmail.com>
2024-12-19 15:39:48 +0200

    Linty stuff

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,38 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import js from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin-js";
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+    },
+    plugins: {
+      "@stylistic/js": stylisticJs,
+    },
+    rules: {
+      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/linebreak-style": ["error", "windows"],
+      "@stylistic/js/quotes": ["error", "single"],
+      "@stylistic/js/semi": ["error", "never"],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true },
+      ],
+      'no-console': 'off',
+    },
+  },
+  { 
+    ignores: ["dist/**", "build/**"],
+  },
 ];
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
-const cors = require("cors");
-const Person = require("./models/person");
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+const Person = require('./models/person')
 
-app.use(cors());
-app.use(express.static("dist"));
-app.use(express.json());
+app.use(cors())
+// app.use(express.static("dist"));
+app.use(express.json())
 
-morgan.token("body", (req) => {
-  return JSON.stringify(req.body);
-});
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
 
 app.use(
   morgan(function (tokens, req, res) {
@@ -19,125 +19,118 @@ app.use(
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
       tokens.body(req, res),
-    ].join(" ");
+    ].join(' ')
   })
-);
+)
 
-app.get("/info", (request, response, next) => {
+app.get('/info', (request, response, next) => {
   Person.find({})
     .then((people) => {
       people.forEach((person) => {
-        console.log(person);
-      });
+        console.log(person)
+      })
       response.send(
         `<p>Phonebook has info for ${
           people.length
         } people</p><p>${new Date()}</p>`
-      );
+      )
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/persons", (request, response, next) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then((people) => {
       people.forEach((person) => {
-        console.log(person);
-      });
+        console.log(person)
+      })
 
-      response.json(people);
+      response.json(people)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   return Person.findById({ _id: request.params.id })
     .then((person) => {
       if (person) {
-        return response.json(person);
+        return response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  console.log(request.body);
-  const body = request.body;
-
-  if (!body.number) {
-    return response
-      .status(400)
-      .json({ error: "Number is needed to update an entry" });
-  }
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
   const person = {
     name: body.name,
     number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then((updatedPerson) => {
-      response.json(updatedPerson);
-    })
-    .catch((error) => next(error));
-});
-
-app.post("/api/persons/", (request, response, next) => {
-  const body = request.body;
-
-  if (!body.name || !body.number) {
-    return response
-      .status(400)
-      .json({ error: "Both name and number are needed to add a new entry" });
   }
+
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
+})
+
+app.post('/api/persons/', (request, response, next) => {
+  const body = request.body
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person
     .save()
     .then((person) => {
-      response.json(person);
+      response.json(person)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete({ _id: request.params.id })
     .then(() => {
-      response.status(204).end();
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
