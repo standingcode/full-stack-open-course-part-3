11a585beef3967c37d7aee9190eaf5a0154dbdfe
Paul Standing <paultobiasstanding@gmail.com>
2024-12-19 12:29:47 +0200

    Phonebook backend now working with database, might need some tweaking still regarding how to handle post/puts

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
+const Person = require("./models/person");
 
 app.use(cors());
-app.use(express.static("dist"));
+// app.use(express.static("dist"));
 app.use(express.json());
 
 morgan.token("body", (req) => {
@@ -26,79 +28,83 @@ app.use(
   })
 );
 
-let entries = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${entries.length} people</p><p>${new Date()}</p>`
-  );
+  Person.find({})
+    .then((people) => {
+      people.forEach((person) => {
+        console.log(person);
+      });
+      response.send(
+        `<p>Phonebook has info for ${
+          people.length
+        } people</p><p>${new Date()}</p>`
+      );
+    })
+    .catch((error) => {
+      console.log(error);
+      response.send("Could not count the users");
+    });
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(entries);
+  Person.find({})
+    .then((people) => {
+      people.forEach((person) => {
+        console.log(person);
+      });
+
+      response.json(people);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const entry = entries.find((entry) => entry.id === id);
-
-  if (entry) {
-    response.json(entry);
-  } else {
-    response.status(404).end();
-  }
+  return Person.findById(request.params.id)
+    .then((person) => {
+      console.log(person.name);
+      return response.json(person);
+    })
+    .catch((error) => {
+      return response.status(404).end();
+    });
 });
-
-const generateId = () => {
-  const maxId =
-    entries.length > 0 ? Math.max(...entries.map((n) => Number(n.id))) : 0;
-  return String(Math.random() * 1000000);
-};
 
 app.put("/api/persons/:id", (request, response) => {
   console.log(request.body);
   const body = request.body;
 
-  if (!body.name || !body.number) {
+  if (!body.number) {
     return response
       .status(400)
-      .json({ error: "Both name and number are needed to update an entry" });
+      .json({ error: "Number is needed to update an entry" });
   }
 
-  const existingPerson = entries.find((entry) => entry.name === body.name);
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        person.number = body.number;
 
-  if (!existingPerson) {
-    return response
-      .status(400)
-      .json({ error: "The user did not exist in the phonebook" });
-  }
-
-  const updatePersonObject = { ...existingPerson, number: body.number };
-
-  entries = entries.filter((entry) => entry.id !== updatePersonObject.id);
-  entries = entries.concat(updatePersonObject);
-  response.json(updatePersonObject);
+        person
+          .save()
+          .then((person) => {
+            response.json(person);
+          })
+          .catch((error) => {});
+      } else {
+        console.log("The updated user could not be saved");
+        return response
+          .status(400)
+          .json({ error: "The updated user could not be saved" });
+      }
+    })
+    .catch((error) => {
+      console.log("Could not get person by id");
+      return response
+        .status(400)
+        .json({ error: "A current user could not be found with this id" });
+    });
 });
 
 app.post("/api/persons/", (request, response) => {
@@ -110,28 +116,30 @@ app.post("/api/persons/", (request, response) => {
       .json({ error: "Both name and number are needed to add a new entry" });
   }
 
-  if (entries.find((entry) => entry.name === body.name)) {
-    return response
-      .status(400)
-      .json({ error: "The name already exists in the phonebook" });
-  }
-
-  const entry = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  entries = entries.concat(entry);
-
-  response.json(entry);
+  person
+    .save()
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => {
+      console.log("Could not save new person");
+      return response.status(400).json({ error: "Could not save new person" });
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  entries = entries.filter((entry) => entry.id !== id);
-
-  response.status(204).end();
+  Person.deleteOne({ _id: request.params.id })
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      response.status(204).end();
+    });
 });
 
 const unknownEndpoint = (request, response) => {
@@ -140,7 +148,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
